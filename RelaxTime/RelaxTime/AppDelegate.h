5eaf4ec33f2bb579d7555a7950a9d363f9b1603d
songyang <447523382@qq.com>
2016-06-29 17:04:51 +0800

    创建工程

---
+++ RelaxTime/RelaxTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RelaxTime
+//
+//  Created by 千锋 on 16/6/29.
+//  Copyright © 2016年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RelaxTime/RelaxTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RelaxTime
+//
+//  Created by 千锋 on 16/6/29.
+//  Copyright © 2016年 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
